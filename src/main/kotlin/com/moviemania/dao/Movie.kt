8ab4dea2f5ab04b864b51550880baf019a24ffee
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2023-05-27 17:23:58 +0200

    Add Movie data class

---
+++ src/main/kotlin/com/moviemania/dao/Movie.kt
@@ -0,0 +1,38 @@
+package com.moviemania.dao
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+
+@JsonIgnoreProperties(
+    value = [
+        "Rated",
+        "Released",
+        "Runtime",
+        "Genre",
+        "Director",
+        "Writer",
+        "Actors",
+        "Plot",
+        "Language",
+        "Country",
+        "Awards",
+        "Poster",
+        "Ratings",
+        "Metascore",
+        "imdbRating",
+        "imdbVotes",
+        "imdbID",
+        "Type",
+        "DVD",
+        "BoxOffice",
+        "Production",
+        "Website",
+        "Response"
+    ]
+)
+data class Movie(
+    @JsonProperty("Title")
+    var title: String,
+    @JsonProperty("Year")
+    var year: Int
+)
